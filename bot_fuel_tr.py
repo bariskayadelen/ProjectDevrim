94d1b9cbfdc0463d23dbb4ebb133e61bc23ba257
Baris Kayadelen <baris@kayadelen.com>
2022-06-13 17:00:54 +0300

    DB function update

--- bot_fuel_tr.py
+++ bot_fuel_tr.py
@@ -1,9 +1,31 @@
-# Web sayfasından otomatik akaryakıt fiyatlarını alan script
+# Fuel Price Watcher Script
 
+import datetime
 import urllib.request
 from bs4 import BeautifulSoup as soup
+import sqlite3
 
+def FuelTr_update(date,comp,fuel,diesel,lpg):
+    con = sqlite3.connect("unitprices.db")
+    cursor = con.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS FuelTr(Date TEXT,CompanyName TEXT,GasPrice REAL,Diesel REAL,LPG REAL)")
+    con.commit()
+    data = cursor.execute("SELECT * FROM FuelTr ORDER BY Date DESC LIMIT 1")
+    data = cursor.fetchone()[0]
+    if data == date:
+        print ("Bugün için veri girişi yapılmış.")
+    else:
+        insert_with_param = """INSERT INTO FuelTr (Date,CompanyName,GasPrice,Diesel,LPG) VALUES (?, ?, ?, ?, ?);"""
+        data_tuple = (date,comp,fuel,diesel,lpg)
+        cursor.execute(insert_with_param, data_tuple)
+        con.commit()
+        print ("Yeni veri girişi yapıldı.")
+    con.close()
+
+today = str(datetime.date.today())
 url = "https://www.aytemiz.com.tr/"
+company = "Aytemiz Petrol"
+
 req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'})
 uClient = urllib.request.urlopen(req)
 page_html = uClient.read()
@@ -14,7 +36,4 @@ fuel_price = page_soup.find_all('div', {"class":"fuel-price"})[0].text.strip().s
 diesel_price = page_soup.find_all('div', {"class":"fuel-price"})[1].text.strip().strip('TL/LT')
 lpg_price = page_soup.find_all('div', {"class":"fuel-price"})[3].text.strip().strip('TL/LT')
 
-print(f"\nBenzin Fiyatı: {fuel_price}  |  Dizel Fiyatı: {diesel_price}  |  LPG Fiyatı: {lpg_price}\n")
-
-bdm = float(fuel_price) * 54
-print(f"Bir depo dolum maliyeti: {bdm} TL'dir.\n")
+FuelTr_update(today,company,fuel_price,diesel_price,lpg_price)
--- test.py
+++ test.py
@@ -1,7 +1,45 @@
+# Web sayfasından otomatik akaryakıt fiyatlarını alan script
+
+import datetime
 import urllib.request
 from bs4 import BeautifulSoup as soup
+import sqlite3
 
+# def FuelTr_show():
+#     con = sqlite3.connect("unitprices.db")
+#     cursor = con.cursor()
+#     cursor.execute("CREATE TABLE IF NOT EXISTS FuelTr(Date TEXT,CompanyName TEXT,GasPrice REAL,Diesel REAL,LPG REAL)")
+#     con.commit()
+#     cursor.execute("SELECT * FROM FuelTr")
+#     data = cursor.fetchall()
+#     print(f"\n Tarih  \tŞirket  \tBenzin Fiyatı\tDizel Fiyatı\tLPG Fiyatı")
+#     for i in data:
+#         print(f" {i[0]}\t{i[1]}\t{i[2]}\t\t{i[3]}\t\t{i[4]}")
+#     con.close()
+
+def FuelTr_update(date,comp,fuel,diesel,lpg):
+    con = sqlite3.connect("unitprices.db")
+    cursor = con.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS FuelTr(Date TEXT,CompanyName TEXT,GasPrice REAL,Diesel REAL,LPG REAL)")
+    con.commit()
+    cursor.execute("SELECT * FROM FuelTr")
+    data = cursor.fetchall()
+    for i in data:
+        if i[0] == today:
+            print ("Bugun veri girisi yapilmis.")
+        else:
+            # print(f" {date}\t{comp}\t{fuel}\t\t{diesel}\t\t{lpg}")
+            # cursor.execute("INSERT INTO FuelTr (Date,CompanyName,GasPrice,Diesel,LPG) VALUES(date,comp,fuel,diesel,lpg)")
+            insert_with_param = """INSERT INTO FuelTr (Date,CompanyName,GasPrice,Diesel,LPG) VALUES (?, ?, ?, ?, ?);"""
+            data_tuple = (date,comp,fuel,diesel,lpg)
+            cursor.execute(insert_with_param, data_tuple)
+            con.commit()
+    con.close()
+
+today = datetime.date.today()
 url = "https://www.aytemiz.com.tr/"
+company = "Aytemiz Petrol"
+
 req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'})
 uClient = urllib.request.urlopen(req)
 page_html = uClient.read()
@@ -11,8 +49,5 @@ page_soup = soup(page_html.decode('utf-8','ignore').encode("utf-8"), 'html5lib')
 fuel_price = page_soup.find_all('div', {"class":"fuel-price"})[0].text.strip().strip('TL/LT')
 diesel_price = page_soup.find_all('div', {"class":"fuel-price"})[1].text.strip().strip('TL/LT')
 lpg_price = page_soup.find_all('div', {"class":"fuel-price"})[3].text.strip().strip('TL/LT')
-# fuel_price = page_soup.find_all('div', {"class":"fuel-price"})[0].text.strip().strip('TL/LT')
-# x1 = fuel_price.text
 
-# print(x1)
-print(fuel_price, diesel_price, lpg_price)
+FuelTr_update(today,company,fuel_price,diesel_price,lpg_price)
