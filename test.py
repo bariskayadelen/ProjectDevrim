63863b4ea8d47cd522e67b1cec31dd60258c19bb
Baris Kayadelen <baris@kayadelen.com>
2022-06-21 02:13:00 +0300

    Fuel Car menu is active

--- test.py
+++ test.py
@@ -112,6 +112,38 @@ def calc_ElectricityTr(power,elec_price,dist_price):
     total = pre_vat + vat
     return total
 
+def calc_fuel_cost(fuel_type,tank_size):
+    con = sqlite3.connect("unitprices.db")
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM FuelTr ORDER BY Date DESC LIMIT 1")
+    data = cursor.fetchall()
+    for i in data:
+        if fuel_type == "Benzin":
+            return(int(i[2]) * tank_size)
+        elif fuel_type == "Diesel":
+            return(int(i[3]) * tank_size)
+        elif fuel_type == "LPG":
+            return(int(i[4]) * tank_size)
+        else:
+            return None
+    con.close()
+
+def calc_fuel_cost_per_km(fuel_type,consumption):
+    con = sqlite3.connect("unitprices.db")
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM FuelTr ORDER BY Date DESC LIMIT 1")
+    data = cursor.fetchall()
+    for i in data:
+        if fuel_type == "Benzin":
+            return(int(i[2]) * consumption / 100)
+        elif fuel_type == "Diesel":
+            return(int(i[3]) * consumption / 100)
+        elif fuel_type == "LPG":
+            return(int(i[4]) * consumption / 100)
+        else:
+            return None
+    con.close()
+
 def calc_row_number(inp):
     if inp == "ElectricityTr":
         con = sqlite3.connect("unitprices.db")
@@ -123,6 +155,12 @@ def calc_row_number(inp):
         con = sqlite3.connect("car.db")
         cursor = con.cursor()
         cursor.execute("SELECT COUNT(*) FROM ElectricCar")
+        row_number = cursor.fetchone()[0]
+        return row_number
+    elif inp == "FuelCar":
+        con = sqlite3.connect("car.db")
+        cursor = con.cursor()
+        cursor.execute("SELECT COUNT(*) FROM FuelCar")
         row_number = cursor.fetchone()[0]
         return row_number
     else:
@@ -158,6 +196,14 @@ def find_electric_car(carid):
 def find_electric_car_brand():
     pass
 
+def find_fuel_car(carid):
+    con = sqlite3.connect("car.db")
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM FuelCar WHERE CarID=?", (carid,))
+    data = cursor.fetchall()
+    con.close()
+    return data[0]
+
 def show_all_cars():
     # https://ev-database.org/
     con = sqlite3.connect("car.db")
@@ -171,19 +217,19 @@ def show_all_cars():
     con.close()
 
 # !!! 
-# def show_all_electric_car_brands():
-#     con = sqlite3.connect("car.db")
-#     cursor = con.cursor()
-#     cursor.execute("SELECT DISTINCT(CarBrand) FROM ElectricCar")
-#     data = cursor.fetchall()
-#     dict_brand = []
-#     count = 0
-#     print(f"\n ID\tMarka")
-#     print(f" {'':-^{5}}  {'':-^{10}}")
-#     for i in data:
-#         dict_brand.append(count, {i[0]})
-#         # print(f" {i[0]}")
-#     con.close()
+def show_all_electric_car_brands():
+    con = sqlite3.connect("car.db")
+    cursor = con.cursor()
+    cursor.execute("SELECT DISTINCT(CarBrand) FROM ElectricCar")
+    data = cursor.fetchall()
+    dict_brand = []
+    count = 0
+    print(f"\n ID\tMarka")
+    print(f" {'':-^{5}}  {'':-^{10}}")
+    for i in data:
+        dict_brand.append(count, {i[0]})
+        # print(f" {i[0]}")
+    con.close()
 
 # !!! 
 def show_all_hibrid_car_brands():
@@ -345,7 +391,47 @@ while True:
         print(f"\n Akaryakıt maliyetini hesaplamak istediğiniz aracın ID kodunu giriniz. ")
         menu_car_fuel()
         print(f"\n{'':-^{tbl_len_out}}")
-        if menu_bottom() == "break": break
+        inp_menu13 = input(f"\n [A] Ana menüye dön | [Q] Programdan Çık | Tercih: ")
+        try:
+            menu13 = int(inp_menu13)
+            if menu13 in range(1,(calc_row_number("FuelCar") + 1)):
+                car_data = find_fuel_car(inp_menu13)
+                fuel_cost = calc_fuel_cost(car_data[7],car_data[8])
+                clear()
+                print(menu_title())
+                print(f"\n {'Marka':{tbl_len_car}}: {car_data[1]}")
+                print(f" {'Model':{tbl_len_car}}: {car_data[2]}")
+                print(f" {'Motor':{tbl_len_car}}: {car_data[3]}")
+                print(f" {'Model yılı':{tbl_len_car}}: {car_data[4]}")
+                print(f"\n {'Motor hacmi':{tbl_len_car}}: {car_data[5]} cc")
+                print(f" {'Motor gücü':{tbl_len_car}}: {car_data[6]} kWh")
+                print(f" {'Yakıt Tipi':{tbl_len_car}}: {car_data[7]}")
+                print(f" {'Akaryakıt depo hacmi':{tbl_len_car}}: {car_data[8]} lt")
+                print(f"\n {'Tam Depo Dolum Ücreti':{tbl_len_car}}: {fuel_cost} ₺")
+                print(f"\n {'23°C Hava Sıcaklığında':{tbl_len_car}}  Yakıt Tük.\tKm Maliyeti")
+                print(f" {'':-^{tbl_len_car}}  {'':-^{13}}  {'':-^{13}}")
+                print(f" {'Fabrika Menzili Şehiriçi':{tbl_len_car}}: {car_data[9]} l/100km\t{round(calc_fuel_cost_per_km(car_data[7],car_data[9]),2)} ₺")
+                print(f" {'Fabrika Menzili Şehirdışı':{tbl_len_car}}: {car_data[10]} l/100km\t{round(calc_fuel_cost_per_km(car_data[7],car_data[10]),2)} ₺")
+                print(f" {'Fabrika Menzili Karma':{tbl_len_car}}: {car_data[11]} l/100km\t{round(calc_fuel_cost_per_km(car_data[7],car_data[11]),2)} ₺")
+                # print(f" {'Kullanıcı Menzili Şehiriçi':{tbl_len_car}}: {car_data[10]}km\t{round(cost_charge100/car_data[10],2)} ₺")
+                # print(f" {'Kullanıcı Menzili Şehirdışı':{tbl_len_car}}: {car_data[11]}km\t{round(cost_charge100/car_data[11],2)} ₺")
+                print(f" {'Kullanıcı Menzili Karma':{tbl_len_car}}: {car_data[14]} l/100km\t{round(calc_fuel_cost_per_km(car_data[7],car_data[14]),2)} ₺")
+                print(f"\n{'':-^{tbl_len_out}}")
+                if menu_bottom() == "break": break
+        except:
+            if inp_menu13.lower() == "q":
+                print(f"\n{' İyi Günler ':=^{tbl_len_out}}\n")
+                break
+            elif inp_menu13.lower() == "a":
+                continue
+            else:
+                clear()
+                print(menu_title())
+                menu_error(inp_menu13)
+                print(f"\n{'':-^{tbl_len_out}}")
+                if menu_bottom() == "break": break
+
+        # if menu_bottom() == "break": break
 
     # [21] Araç bilgisi
     elif inp_mainmenu == "21":
