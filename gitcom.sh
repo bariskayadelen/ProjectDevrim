b91789076a254254d550c7b2a053f725d3161c01
bariskayadelen <baris@kayadelen.com>
2023-01-09 15:06:03 +0000

    2023-01-09 Fuel Update

---
+++ gitcom.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Chek fuel prices
+python3 bot_fuel_tr.py
+
+# Waits 30 seconds.
+sleep 30
+
+# Add to git repository
+git add .
+git commit -m "`date +%Y-%m-%d` Fuel Update"
+#git push origin main
