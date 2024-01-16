5a75fc62e61d65d5a519cb220e56eab1dacb4566
Baris Kayadelen <baris@kayadelen.com>
2024-01-16 17:04:39 +0300

    rename gas

--- get_fuelprice_tr.py
+++ get_fuelprice_tr.py
@@ -15,7 +15,7 @@ header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWe
 req = requests.get(url, headers=header)
 page_soup = soup(req.content.decode('utf-8','ignore').encode("utf-8"), 'html5lib')
 fuel_district = page_soup.find_all('td', {"data-title":"DISTRICT"})[0].text.strip()
-fuel_price = page_soup.find_all('td', {"data-title":"UNLEADED GASOLINE (TL/LT)"})[0].text.strip()
+gas_price = page_soup.find_all('td', {"data-title":"UNLEADED GASOLINE (TL/LT)"})[0].text.strip()
 diesel_price = page_soup.find_all('td', {"data-title":"TP DIESEL (TL/LT)"})[0].text.strip()
 lpg_price = page_soup.find_all('td', {"data-title":"TPGAS"})[0].text.strip()
 
@@ -31,7 +31,7 @@ def main():
     print(f"{' City':<{tbl_len_in}} : {fuel_district}")
     print(f"{' Date':<{tbl_len_in}} : {today}")
     print(f"{' Time':<{tbl_len_in}} : {time}")
-    print(f"\n{' Fuel':<{tbl_len_in}} : {fuel_price} ₺")
+    print(f"\n{' Fuel':<{tbl_len_in}} : {gas_price} ₺")
     print(f"{' Diesel':<{tbl_len_in}} : {diesel_price} ₺")
     print(f"{' LPG':<{tbl_len_in}} : {lpg_price} ₺")
     print(f"\n{'':=^{tbl_len_out}}\n")
